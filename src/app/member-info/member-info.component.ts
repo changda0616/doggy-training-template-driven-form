6fedf880297bcca25caddb5625685a6b1a94955d
chad <changda0616@gmail.com>
2020-01-14 16:26:23 +0800

    元件、資料初始化

---
+++ src/app/member-info/member-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-info',
+  templateUrl: './member-info.component.html',
+  styleUrls: ['./member-info.component.scss']
+})
+export class MemberInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
